19f892c48ba80eee7b5f23116ecad84191210726
mirco <mirco.pl.93@gmail.com>
2021-11-20 11:23:32 +0100

    fix loader

--- gtea_dataset.py
+++ gtea_dataset.py
@@ -84,7 +84,7 @@ class GTEA61(VisionDataset):
         # we expect datadir to be GTEA61, so we add FRAME_FOLDER to get to the frames
         frame_dir = os.path.join(self.datadir, FRAME_FOLDER)
         users = os.listdir(frame_dir)
-        if len(users) != 4:
+        if len(users) < 4:
             raise FileNotFoundError("you specified the wrong directory")
         if TEST_USER not in users:
             raise FileNotFoundError("S2 folder not found")
